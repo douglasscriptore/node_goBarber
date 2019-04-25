2b159a65bfbaa35a2352754d2939504a00592b66
Douglas Scriptore <douglass@rpc.com.br>
2019-04-24 23:33:32 -0300

    adicionando funcionalidade para os prestadores de serviço visualizarem suas agendas

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,8 +1,25 @@
-const { User } = require('../models')
+const { User, Appointment } = require('../models')
+const { Op } = require('sequelize')
+const moment = require('moment')
 
 class DashboardController {
   async index (req, res) {
+    const date = moment(new Date())
+    console.log(date)
     const providers = await User.findAll({ where: { provider: true } })
+
+    if (req.session.user.provider === true) {
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: req.session.user.id,
+          date: {
+            [Op.gte]: date.startOf('day').format()
+          }
+        },
+        include: [{ model: User, as: 'user' }]
+      })
+      return res.render('dashboard', { appointments })
+    }
 
     return res.render('dashboard', { providers })
   }
--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,11 +1,31 @@
+const moment = require('moment')
+
 module.exports = (sequelize, DataTypes) => {
-  const Appointment = sequelize.define('Appointment', {
-    date: DataTypes.DATE
-  })
+  const Appointment = sequelize.define(
+    'Appointment',
+    {
+      date: DataTypes.DATE,
+      date_formated: DataTypes.VIRTUAL
+    },
+    {
+      hooks: {
+        afterFind: appointment => {
+          if (appointment.constructor === Array) {
+            appointment.map(ap => {
+              ap.date_formated = moment(ap.date).format('D/M/Y HH:mm')
+            })
+          }
+        }
+      }
+    }
+  )
 
   Appointment.associate = models => {
-    Appointment.belongsTo(models.User, { foreignKey: 'user_id' })
-    Appointment.belongsTo(models.User, { foreignKey: 'provider_id' })
+    Appointment.belongsTo(models.User, { as: 'user', foreignKey: 'user_id' })
+    Appointment.belongsTo(models.User, {
+      as: 'provider',
+      foreignKey: 'provider_id'
+    })
   }
 
   return Appointment
